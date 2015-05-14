650058296c396e86e63697d5d5a25fbd4d5308fd
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-05-14 15:32:19 +0900

    Add #!/usr/bin/env node

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
+#!/usr/bin/env node
 var pictureTube = require('picture-tube')
 var tube = pictureTube({cols: 65});
 tube.pipe(process.stdout);
 
 var fs = require('fs');
 fs.createReadStream('img/ymzkmct.png').pipe(tube);
-
-module.exports = function (cb) {
-  tube.pipe(cb);
-};
