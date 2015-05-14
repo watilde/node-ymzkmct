7302c644a6902bcad28bd08f70eb72a7c3d10467
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-05-14 15:30:31 +0900

    Add ymzkmct command

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+var pictureTube = require('picture-tube')
+var tube = pictureTube({cols: 65});
+tube.pipe(process.stdout);
 
+var fs = require('fs');
+fs.createReadStream('img/ymzkmct.png').pipe(tube);
+
+module.exports = function (cb) {
+  tube.pipe(cb);
+};
